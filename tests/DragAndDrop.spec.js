6f0008f3495e3493a7a682e2ce3d4fb782e1daf5
unknown <nileshkasane98@gmail.com>
2025-09-15 18:31:21 +0530

    Drag and Drop Action

---
+++ tests/DragAndDrop.spec.js
@@ -0,0 +1,26 @@
+import { test, expect } from "@playwright/test"
+
+test("Drag and Drop Action", async ({ page }) => {
+
+    await page.goto("https://testautomationpractice.blogspot.com/");
+
+    const dragElement = await page.locator('//div[@class="ui-widget-content ui-draggable ui-draggable-handle"]');
+
+    const dropElement = await page.locator('//div[@class="ui-widget-header ui-droppable"]');
+
+    // Approach 1
+    await dragElement.hover();
+    await page.mouse.down();  //Click and and hold the mouse button
+
+    await dropElement.hover();
+    await page.mouse.up();  //Releasing the mouse 
+
+
+    //Approach 2
+    await dragElement.dragTo(dropElement);  //simple apprach to do this 
+
+
+    await page.waitForTimeout(5_000);
+
+
+})
