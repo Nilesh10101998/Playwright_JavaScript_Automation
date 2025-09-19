87a86b03209667f845cc692f477216c53f9f4c45
unknown <nileshkasane98@gmail.com>
2025-09-19 14:21:23 +0530

    Record video and Trace Viewer

---
+++ tests/Tracing.spec.js
@@ -0,0 +1,14 @@
+import { test, expect } from "@playwright/test"
+
+test("Record Test Video", async ({ page }) => {
+
+    await page.goto("https://demoblaze.com/index.html");
+
+    //Login
+    await page.locator("#login2").click();
+    await page.locator("#loginusername").fill("Nilesh Kasane");
+    await page.locator("#loginpassword").fill("Practice@123");
+    await page.locator("//button[normalize-space()='Log in']").click();
+
+    await expect(page.locator("#logout2")).toBeVisible();
+})
